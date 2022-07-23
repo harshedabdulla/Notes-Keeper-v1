66cd6def5d768e5c6246921cd69cb3b9eb447272
harshedabdulla <harshed2004@gmail.com>
2022-07-23 10:34:04 +0530

    fd

---
+++ src/Components/docs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+export default function Docs() {
+  return (
+    <div>
+        <div className='docs-main'>
+        <h1>Notes Keeper</h1>
+        </div>
+    </div>
+  )
+}
